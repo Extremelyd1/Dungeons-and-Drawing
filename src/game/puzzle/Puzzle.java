bdd65fcb0ad0e7d2827510f656ea8a40452e717f
Casper Smits <caspersmits11@outlook.com>
2018-09-29 15:52:06 +0200

    created action and solution

--- src/game/puzzle/Puzzle.java
+++ src/game/puzzle/Puzzle.java
@@ -1,26 +1,40 @@
 package game.puzzle;
 
+import game.action.Action;
+
 public abstract class Puzzle {
 
     private String description;
-    private float time;
     private String[] options;
+    private Solution[] solutions;
+    private float time;
 
-    public Puzzle(String description, float time, String[] options) {
+    public Puzzle(String description, String[] options, Solution[] solutions, float time) {
         this.description = description;
-        this.time = time;
         this.options = options;
+        this.solutions = solutions;
+        this.time = time;
     }
 
     public String getDescription() {
         return description;
     }
 
+    public String[] getOptions() {
+        return options;
+    }
+
+    public Solution[] getSolutions() {
+        return solutions;
+    }
+
     public float getTime() {
         return time;
     }
 
-    public String[] getOptions() {
-        return options;
+    public Action evaluate() {
+        // TODO: Evaluate image and return appropiate action
+
+        return null; // TODO: Replace with action
     }
 }
