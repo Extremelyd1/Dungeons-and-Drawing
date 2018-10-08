6d18177755598a7f69410fc6f2f402f49c569d26
cwognum <c.wognum@student.tue.nl>
2018-10-08 10:42:21 +0200

    Deleted Menu code I forgot

--- src/game/GUI.java
+++ src/game/GUI.java
@@ -22,13 +22,8 @@ public class GUI {
 
     public void initialize() {
         nano = NanoVG.getInstance();
-        components.add(new Menu(null));
         components.add(new Popup("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "));
 //        components.add(new Button("Crash the game!", null));
-
-        Popup p = new Popup();
-        components.add(p);
-        components.remove(p);
 
 //        TitleCard titleCard = new TitleCard();
 //        titleCard.setPosition(650, 100);
